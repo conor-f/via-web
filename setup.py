3eb33cf4f5423080969bae70a3d0fa9d0d7789b6
Conor Flynn <conor@glimpse.ie>
2021-11-05 18:32:11 +0000

    Bumps to 0.1.3

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ INSTALL_REQUIRES = (
 
 setup(
     name='via-web',
-    version='0.1.2',
+    version='0.1.3',
     python_requires='>=3.6',
     description='Analysing and serving crowdsourced road quality data',
     author='Conor Flynn',
