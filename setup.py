a10d4646dc99d42fec11fea8a271b8757da647a5
Robert Lucey <robert@glimpse.ie>
2022-06-16 14:44:32 +0100

    Bump via-api (0.0.50)

--- setup.py
+++ setup.py
@@ -11,13 +11,13 @@ INSTALL_REQUIRES = (
     'boto3',
     'bottle',
     'gunicorn',
-    'via-api==0.0.47',
+    'via-api==0.0.50',
     'osmnx'
 )
 
 setup(
     name='via-web',
-    version='0.1.11',
+    version='0.1.12',
     python_requires='>=3.6',
     description='Analysing and serving crowdsourced road quality data',
     author='Conor Flynn',
